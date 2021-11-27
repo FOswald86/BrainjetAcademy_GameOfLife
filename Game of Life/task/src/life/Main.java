15461eb9daddd06a0ca477009b29812241a721b8
easy-coding <86290835+FOswald86@users.noreply.github.com>
2021-11-27 17:15:35 +0100

    Add files via upload

--- Game of Life/task/src/life/Main.java
+++ Game of Life/task/src/life/Main.java
@@ -1,16 +1,57 @@
 package life;
 
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
-        
+    public static void main(String[] args) {
         Universe universe = new Universe();
-        universe.populate(universe);
+        Generation generation = new Generation(universe);
+        universe.setCurrentState(generation.generationZero());
+        GameOfLife gameOfLife = new GameOfLife();
 
-        // while button play/toggle
         while (true) {
-            Universe universeNextGen = new Universe();
-            universe = universe.evolve(universe, universeNextGen);
-            Thread.sleep(1000);
+            // Reset button pressed
+            if (gameOfLife.isReset()) {
+                universe = new Universe();
+                generation = new Generation(universe);
+                generation.generationZero();
+                gameOfLife.reset();
+            }
+
+            // Pause/Play button pressed
+            if (gameOfLife.isPaused()) {
+                try {
+                    Thread.sleep(1);
+                    continue;
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+
+            generation.nextGeneration();
+            char[][] currentState = universe.getCurrentState();
+
+            for (int i = 0; i < currentState.length; i++) {
+                for (int j = 0; j < currentState.length; j++) {
+                    if (currentState[i][j] == 'O') {
+                        gameOfLife.grid.fillCell(i, j);
+                    } else {
+                        gameOfLife.grid.removeCell(i, j);
+                    }
+                }
+            }
+
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                System.out.println(e);
+            }
+
+            String gen = "Generation #" + gameOfLife.getCurrentGeneration();
+            String alive = "Alive: " + generation.getAliveCount(currentState);
+
+            gameOfLife.GenerationLabel.setText(gen);
+            gameOfLife.AliveLabel.setText(alive);
+
+            gameOfLife.nextGeneration();
         }
     }
 }
